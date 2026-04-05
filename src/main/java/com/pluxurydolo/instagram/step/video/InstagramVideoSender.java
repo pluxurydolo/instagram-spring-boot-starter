8403a37b48751a565ced2d3ba8d0d8aa6e2cb12b
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-05 23:08:23 +0300

    05.04.2026

---
+++ src/main/java/com/pluxurydolo/instagram/step/video/InstagramVideoSender.java
@@ -0,0 +1,69 @@
+package com.pluxurydolo.instagram.step.video;
+
+import com.pluxurydolo.instagram.dto.Token;
+import com.pluxurydolo.instagram.dto.request.upload.CreateContainerRequest;
+import com.pluxurydolo.instagram.dto.request.upload.ContainerStatusRequest;
+import com.pluxurydolo.instagram.dto.request.upload.PublishContainerRequest;
+import com.pluxurydolo.instagram.dto.request.upload.UploadMediaRequest;
+import com.pluxurydolo.instagram.dto.response.ContainerResponse;
+import com.pluxurydolo.instagram.exception.VideoSenderException;
+import com.pluxurydolo.instagram.properties.InstagramProperties;
+import com.pluxurydolo.instagram.security.token.AbstractTokenRetriever;
+import com.pluxurydolo.instagram.step.InstagramContainerPublisher;
+import com.pluxurydolo.instagram.step.InstagramContainerStatusPoller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+public class InstagramVideoSender {
+    private static final Logger LOGGER = LoggerFactory.getLogger(InstagramVideoSender.class);
+
+    private final InstagramVideoContainerCreator instagramVideoContainerCreator;
+    private final InstagramContainerStatusPoller instagramContainerStatusPoller;
+    private final InstagramContainerPublisher instagramContainerPublisher;
+    private final AbstractTokenRetriever abstractTokenRetriever;
+    private final InstagramProperties instagramProperties;
+
+    public InstagramVideoSender(
+        InstagramVideoContainerCreator instagramVideoContainerCreator,
+        InstagramContainerStatusPoller instagramContainerStatusPoller,
+        InstagramContainerPublisher instagramContainerPublisher,
+        AbstractTokenRetriever abstractTokenRetriever,
+        InstagramProperties instagramProperties
+    ) {
+        this.instagramVideoContainerCreator = instagramVideoContainerCreator;
+        this.instagramContainerStatusPoller = instagramContainerStatusPoller;
+        this.instagramContainerPublisher = instagramContainerPublisher;
+        this.abstractTokenRetriever = abstractTokenRetriever;
+        this.instagramProperties = instagramProperties;
+    }
+
+    public Mono<String> upload(UploadMediaRequest request) {
+        String videoUrl = request.mediaUrl();
+        String caption = request.caption();
+        String userId = instagramProperties.userId();
+
+        return abstractTokenRetriever.retrieve()
+            .map(Token::accessToken)
+            .flatMap(accessToken -> uploadVideo(videoUrl, caption, userId, accessToken))
+            .map(ContainerResponse::id)
+            .doOnSuccess(_ -> LOGGER.info("urue [instagram-starter] Видео успешно опубликовано"))
+            .onErrorResume(throwable -> Mono.error(new VideoSenderException(throwable)));
+    }
+
+    private Mono<ContainerResponse> uploadVideo(String videoUrl, String caption, String userId, String accessToken) {
+        CreateContainerRequest request = new CreateContainerRequest(videoUrl, caption, userId, accessToken);
+
+        return instagramVideoContainerCreator.create(request)
+            .map(ContainerResponse::id)
+            .flatMap(containerId -> publishContainer(containerId, userId, accessToken));
+    }
+
+    private Mono<ContainerResponse> publishContainer(String containerId, String userId, String accessToken) {
+        ContainerStatusRequest request = new ContainerStatusRequest(containerId, accessToken);
+
+        return instagramContainerStatusPoller.pollContainerStatus(request)
+            .map(_ -> new PublishContainerRequest(containerId, userId, accessToken))
+            .flatMap(instagramContainerPublisher::publish);
+    }
+}
---
+++ src/test/java/com/pluxurydolo/instagram/step/video/InstagramVideoSenderTests.java
@@ -0,0 +1,87 @@
+package com.pluxurydolo.instagram.step.video;
+
+import com.pluxurydolo.instagram.dto.Token;
+import com.pluxurydolo.instagram.dto.request.upload.UploadMediaRequest;
+import com.pluxurydolo.instagram.dto.response.ContainerResponse;
+import com.pluxurydolo.instagram.properties.InstagramProperties;
+import com.pluxurydolo.instagram.security.token.AbstractTokenRetriever;
+import com.pluxurydolo.instagram.step.InstagramContainerPublisher;
+import com.pluxurydolo.instagram.step.InstagramContainerStatusPoller;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static reactor.test.StepVerifier.create;
+
+@ExtendWith(MockitoExtension.class)
+class InstagramVideoSenderTests {
+
+    @Mock
+    private InstagramVideoContainerCreator instagramVideoContainerCreator;
+
+    @Mock
+    private InstagramContainerStatusPoller instagramContainerStatusPoller;
+
+    @Mock
+    private InstagramContainerPublisher instagramContainerPublisher;
+
+    @Mock
+    private AbstractTokenRetriever abstractTokenRetriever;
+
+    @Mock
+    private InstagramProperties instagramProperties;
+
+    @InjectMocks
+    private InstagramVideoSender instagramVideoSender;
+
+    @Test
+    void testUpload() {
+        when(instagramProperties.userId())
+            .thenReturn("userId");
+        when(abstractTokenRetriever.retrieve())
+            .thenReturn(Mono.just(token()));
+        when(instagramVideoContainerCreator.create(any()))
+            .thenReturn(Mono.just(containerResponse()));
+        when(instagramContainerStatusPoller.pollContainerStatus(any()))
+            .thenReturn(Mono.just(""));
+        when(instagramContainerPublisher.publish(any()))
+            .thenReturn(Mono.just(containerResponse()));
+
+        Mono<String> result = instagramVideoSender.upload(uploadMediaRequest());
+
+        create(result)
+            .expectNext("id")
+            .verifyComplete();
+    }
+
+    @Test
+    void testUploadWhenExceptionOccurred() {
+        when(instagramProperties.userId())
+            .thenReturn("userId");
+        when(abstractTokenRetriever.retrieve())
+            .thenReturn(Mono.error(new RuntimeException()));
+
+        Mono<String> result = instagramVideoSender.upload(uploadMediaRequest());
+
+        create(result)
+            .expectError(RuntimeException.class)
+            .verify();
+    }
+
+    private static UploadMediaRequest uploadMediaRequest() {
+        return new UploadMediaRequest("mediaUrl", "caption");
+    }
+
+    private static Token token() {
+        return new Token("accessToken");
+    }
+
+    private static ContainerResponse containerResponse() {
+        return new ContainerResponse("id");
+    }
+}
