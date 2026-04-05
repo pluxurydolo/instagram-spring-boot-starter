023c07e71d4bd3cac699bdac7f19b60000691b57
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-05 23:37:08 +0300

    05.04.2026

---
+++ src/main/java/com/pluxurydolo/instagram/step/InstagramContainerStatusPoller.java
@@ -0,0 +1,60 @@
+package com.pluxurydolo.instagram.step;
+
+import com.pluxurydolo.instagram.dto.request.upload.ContainerStatusRequest;
+import com.pluxurydolo.instagram.dto.response.ContainerStatusResponse;
+import com.pluxurydolo.instagram.properties.PollingProperties;
+import com.pluxurydolo.instagram.util.ContainerStatusLogger;
+import com.pluxurydolo.instagram.web.InstagramUploadWebClient;
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class InstagramContainerStatusPoller {
+    private static final Logger LOGGER = LoggerFactory.getLogger(InstagramContainerStatusPoller.class);
+
+    private final InstagramUploadWebClient instagramUploadWebClient;
+    private final ContainerStatusLogger containerStatusLogger;
+    private final PollingProperties pollingProperties;
+
+    public InstagramContainerStatusPoller(
+        InstagramUploadWebClient instagramUploadWebClient,
+        ContainerStatusLogger containerStatusLogger,
+        PollingProperties pollingProperties
+    ) {
+        this.instagramUploadWebClient = instagramUploadWebClient;
+        this.containerStatusLogger = containerStatusLogger;
+        this.pollingProperties = pollingProperties;
+    }
+
+    public Mono<String> poll(ContainerStatusRequest request) {
+        Duration delay = pollingProperties.delay();
+        long delaySeconds = delay.getSeconds();
+        int maxRepeat = pollingProperties.maxRepeat();
+
+        String containerId = request.containerId();
+        String accessToken = request.accessToken();
+
+        Function<Flux<Long>, Publisher<?>> onRepeat = repeat -> repeat
+            .doOnNext(repeatNum -> LOGGER.info(
+                "xwdn [instagram-starter] Повторная попытка обработки контейнера произойдет через {} секунд ({}/{})",
+                delaySeconds, repeatNum + 1, maxRepeat
+            ))
+            .delayElements(delay, Schedulers.boundedElastic());
+
+        return Mono.defer(() -> validateContainerStatus(containerId, accessToken))
+            .repeatWhenEmpty(maxRepeat, onRepeat);
+    }
+
+    private Mono<String> validateContainerStatus(String containerId, String accessToken) {
+        return instagramUploadWebClient.getContainerStatus(containerId, accessToken)
+            .map(ContainerStatusResponse::statusCode)
+            .doOnNext(containerStatusLogger::log)
+            .filter("FINISHED"::equals);
+    }
+}
---
+++ src/test/java/com/pluxurydolo/instagram/step/InstagramContainerStatusPollerTests.java
@@ -0,0 +1,78 @@
+package com.pluxurydolo.instagram.step;
+
+import com.pluxurydolo.instagram.dto.request.upload.ContainerStatusRequest;
+import com.pluxurydolo.instagram.dto.response.ContainerStatusResponse;
+import com.pluxurydolo.instagram.properties.PollingProperties;
+import com.pluxurydolo.instagram.util.ContainerStatusLogger;
+import com.pluxurydolo.instagram.web.InstagramUploadWebClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static reactor.test.StepVerifier.create;
+
+@ExtendWith(MockitoExtension.class)
+class InstagramContainerStatusPollerTests {
+
+    @Mock
+    private InstagramUploadWebClient instagramUploadWebClient;
+
+    @Mock
+    private ContainerStatusLogger containerStatusLogger;
+
+    @Mock
+    private PollingProperties pollingProperties;
+
+    @InjectMocks
+    private InstagramContainerStatusPoller instagramContainerStatusPoller;
+
+    @Test
+    void testPoll() {
+        doNothing()
+            .when(containerStatusLogger).log(anyString());
+        when(pollingProperties.delay())
+            .thenReturn(Duration.ofSeconds(1));
+        when(pollingProperties.maxRepeat())
+            .thenReturn(100);
+        when(instagramUploadWebClient.getContainerStatus(anyString(), anyString()))
+            .thenReturn(Mono.just(containerStatusResponse()));
+
+        Mono<String> result = instagramContainerStatusPoller.poll(createContainerStatusRequest());
+
+        create(result)
+            .expectNext("FINISHED")
+            .verifyComplete();
+    }
+
+    @Test
+    void testPollWhenExceptionOccurred() {
+        when(pollingProperties.delay())
+            .thenReturn(Duration.ofSeconds(1));
+        when(pollingProperties.maxRepeat())
+            .thenReturn(100);
+        when(instagramUploadWebClient.getContainerStatus(anyString(), anyString()))
+            .thenReturn(Mono.error(new RuntimeException()));
+
+        Mono<String> result = instagramContainerStatusPoller.poll(createContainerStatusRequest());
+
+        create(result)
+            .expectError(RuntimeException.class)
+            .verify();
+    }
+
+    private static ContainerStatusRequest createContainerStatusRequest() {
+        return new ContainerStatusRequest("containerId", "accessToken");
+    }
+
+    private static ContainerStatusResponse containerStatusResponse() {
+        return new ContainerStatusResponse("id", "FINISHED", "status");
+    }
+}
