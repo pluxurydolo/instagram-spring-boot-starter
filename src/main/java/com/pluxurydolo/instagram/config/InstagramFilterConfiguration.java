fc1f64d8f861451a9e20ab8d05be1ec26328256a
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-10 18:28:35 +0300

    10.04.2026

--- src/main/java/com/pluxurydolo/instagram/config/InstagramFilterConfiguration.java
+++ src/main/java/com/pluxurydolo/instagram/config/InstagramFilterConfiguration.java
@@ -1,10 +1,14 @@
 package com.pluxurydolo.instagram.config;
 
+import com.pluxurydolo.instagram.filter.InstagramRateLimitFilter;
 import com.pluxurydolo.instagram.filter.InstagramRequestParamValidationFilter;
 import com.pluxurydolo.instagram.properties.InstagramProperties;
+import com.pluxurydolo.instagram.properties.InstagramRateLimitProperties;
 import com.pluxurydolo.instagram.validator.RequestParamValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
 public class InstagramFilterConfiguration {
@@ -15,5 +19,19 @@ public class InstagramFilterConfiguration {
         InstagramProperties instagramProperties
     ) {
         return new InstagramRequestParamValidationFilter(requestParamValidator, instagramProperties);
+    }
+
+    @Bean
+    public InstagramRateLimitFilter instagramRateLimitingFilter(
+        AtomicInteger requestCounter,
+        InstagramProperties instagramProperties,
+        InstagramRateLimitProperties instagramRateLimitProperties
+    ) {
+        return new InstagramRateLimitFilter(requestCounter, instagramProperties, instagramRateLimitProperties);
+    }
+
+    @Bean
+    public AtomicInteger requestCounter() {
+        return new AtomicInteger(0);
     }
 }
