a1f7fb5d22b5a412ef35bedfc82eb5ba8deb05f2
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-05-06 14:27:17 +0300

    06.05.2026

--- src/main/java/com/pluxurydolo/instagram/dto/request/token/AccessTokenRequest.java
+++
@@ -1,8 +0,0 @@
-package com.pluxurydolo.instagram.dto.request.token;
-
-public record AccessTokenRequest(
-    String appId,
-    String appSecret,
-    String exchangeToken
-) {
-}
--- src/main/java/com/pluxurydolo/instagram/dto/request/token/ExchangeTokenRequest.java
+++
@@ -1,9 +0,0 @@
-package com.pluxurydolo.instagram.dto.request.token;
-
-public record ExchangeTokenRequest(
-    String appId,
-    String appSecret,
-    String redirectUri,
-    String code
-) {
-}
